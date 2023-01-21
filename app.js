ffb00f0f92cd42c8ec3c2f4d0c4fddc742116c29
Muneeb Ul Islam <muneebulislam@hotmail.com>
2023-01-21 17:22:19 -0600

    Created a get route for articles

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const https = require('https');
+const ejs = require('ejs');
+const mongoose  = require('mongoose');
 app.use(express.json()); // for parsing application/json instead of body-parser
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.static(path.join(__dirname,'public')))
 
+app.set('view engine', 'ejs');
 
-app.get("/joke", (req,res) => {
-    res.sendFile(__dirname+"/index.html");
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb://localhost:27017/wikiDB");
+
+const articleSchema = {
+    title: String,
+    constent: String
+}
+
+const Article = mongoose.model('Article', articleSchema)
+
+app.get("/articles", (req,res) => {
+    // res.sendFile(__dirname+"/index.html");
+    Article.find((err, article) => {
+        if (err) {console.log(err);
+        } else {
+            res.send(article);
+           
+        }
+    })
 });
 
 
